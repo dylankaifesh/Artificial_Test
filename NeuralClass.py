a9c877f9b04d08863ea2143fd34354bbe0430b85
MoistToast24 <kaifeshd@gmail.com>
2023-07-20 11:10:31 -0500

    Final

--- NeuralClass.py
+++ NeuralClass.py
@@ -69,6 +69,29 @@ class NeuralNetwork:
 
         return cumulative_errors
 
+from matplotlib import pyplot as plt
+
+input_vectors = np.array(
+    [
+        [3, 1.5],
+        [2, 1],
+        [4, 1.5],
+        [3, 4],
+        [3.5, 0.5],
+        [2, 0.5],
+        [5.5, 1],
+        [1, 1],
+    ]
+)
+
+targets = np.array([0, 1, 0, 1, 0, 1, 1, 0])
+
 learning_rate = 0.1
 neural_network = NeuralNetwork(learning_rate)
-print(neural_network.predict(input_vector))
+training_error = neural_network.train(input_vectors, targets, 10000)
+
+plt.plot(training_error)
+plt.xlabel("Iterations")
+plt.ylabel("Error for all training instances")
+plt.savefig("cumulative_error.png")
+
